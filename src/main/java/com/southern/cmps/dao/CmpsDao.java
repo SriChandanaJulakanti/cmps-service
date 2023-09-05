73b5ceecc6a4f489e7cdbfa3d6c3b7e69a0dd470
Sri Chandana Julakanti <srichandana.julak@sus.edu>
2023-09-05 16:58:57 -0500

    changes in CmpsDao

--- src/main/java/com/southern/cmps/dao/CmpsDao.java
+++ src/main/java/com/southern/cmps/dao/CmpsDao.java
@@ -1,5 +1,18 @@
 package com.southern.cmps.dao;
 
-public class CmpsDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class CmpsDao {
+	
+	@Autowired
+	private JdbcTemplate cmpsDaoTemplate;
+	
+	
+	public String getConcentrations() {
+		return cmpsDaoTemplate.queryForObject("SELECT Name FROM cmps.tbl_concentration where ConcentrationCode = '101'", String.class);
+	}
+	
 }
---
+++ src/test/java/com/southern/cmps/dao/test/CmpsDaoTest.java
@@ -0,0 +1,20 @@
+package com.southern.cmps.dao.test;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+import org.springframework.context.ApplicationContext;
+
+import com.southern.cmps.dao.CmpsDao;
+
+public class CmpsDaoTest {
+	ApplicationContext context;
+	CmpsDao cmpsDao = context.getBean(CmpsDao.class);
+	
+//	@Ignore
+	@Test
+	public void getconcentrations() {
+		System.out.println(cmpsDao.getConcentrations());
+	}
+
+}
